f178776a551a84a80f7ea8dee258ad6f5b8f6ab3
Sachin Mahajan <sachinm78@gmail.com>
2019-06-19 17:53:57 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 // your code here
 
+function showRepositories() {
+  //this is set to the XMLHttpRequest object that fired the event
+  console.log(this.responseText);
+}
+ 
 function getRepositories() {
   const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
